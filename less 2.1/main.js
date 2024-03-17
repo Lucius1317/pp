72a98a81c8ddbafa85377d57bced4c04044d9397
Lucius1317 <andree5991@gmail.com>
2024-03-17 21:46:47 +0300

    Less2.3

--- less 2.1/main.js
+++ less 2.1/main.js
@@ -2,10 +2,12 @@ const firstName = 'Kirill'
 const hobby = 'Painting thumbnails'
 
 let lastName = 'Shuvalov'
-let age = '29'
+let age = 29
 let city = 'Yaroslavl'
 
 lastName = 'Andreev'
 
 console.log ('Hello', firstName, lastName, age, city, hobby)
+console.log (typeof firstName)
+
 
---
+++ less 2.2/main.js
@@ -0,0 +1,7 @@
+const titleProject = 'Оценка стоимости'
+let screensValue = 'Шаблонные, с уникальным дизайном, с анимациями'
+let screensPrice = 2500
+let percentage = 30
+let fullPrice = 20000
+let responsive = true
+
